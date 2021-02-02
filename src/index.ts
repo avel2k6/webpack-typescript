cc4c55f6c6ce40aaafd81a0a298a181cba7bab4b
avel2k6 <avel2k6@gmail.com>
2021-02-02 11:14:54 +0300

    Конфиги для TS и базовые упражнения

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
 import './index.css';
+import { ContainerType } from './types/common';
 
 console.log('hello world');
 
 const a = 1 + 2;
 const b = a + 1;
 
-const container = {
+const container: ContainerType = {
     apple: a,
     banana: b,
 };
 
+const stringArray: (string|number)[] = [
+    'apple',
+    'banana',
+    null,
+    '123',
+    123,
+];
+
 const d = container.banana * 4;
 
 console.log(d);
+console.log(stringArray);
---
+++ src/types/common.ts
@@ -0,0 +1,1 @@
+export type ContainerType = { apple: number, banana: number };
